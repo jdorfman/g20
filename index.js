d1acf60a3dc88e7b10a1d6c49ea5bcae65134e12
Justin Dorfman <justindorfman@gmail.com>
2015-12-23 13:54:22 -0800

    index.js clean up

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 require('copy-paste').global()
 var chalk = require('chalk')
 var generatePassword = require('password-generator')
+
 copy(generatePassword(19, false))
 console.log(chalk.bgGreen(paste()) + " has been copied to your clipboard.")
-
-// module.exports = function () {
-// }
