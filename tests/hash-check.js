ab0fdda3d016333214eaf9da21571799270ef04d
Justin Dorfman <justindorfman@gmail.com>
2016-01-16 20:29:54 -0800

    Added tests, .travis.yml and Windows screenshot

---
+++ tests/hash-check.js
@@ -0,0 +1,11 @@
+var cp = require("child_process");
+
+cp.exec("node ./index.js |wc |md5", function (err, stdout, stderr) {
+  var trimStdOut = stdout.trim()
+    if (trimStdOut !== 'c1736c9033135fc399760d2740eb3a7f') {
+      console.log('Fail: The current g20 hash doesn\'t match the correct one.')
+      process.exit(1)
+    } else {
+      console.log('Pass')
+    }
+});
