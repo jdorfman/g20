b24c491110bc4bd3fc2809f9b7b3b72c077516c5
Justin Dorfman <justindorfman@gmail.com>
2017-07-09 10:58:11 -0700

    Removed green background from generated password

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ var chalk = require('chalk')
 var generatePassword = require('password-generator')
 
 clipboardy.writeSync(generatePassword(19, false))
-console.log("\r\n" + chalk.bgGreen(clipboardy.readSync()) + " has been copied to your clipboard." + "\r\n")
+console.log("\r\n" + chalk.green.bold(clipboardy.readSync()) + " has been copied to your clipboard." + "\r\n")
